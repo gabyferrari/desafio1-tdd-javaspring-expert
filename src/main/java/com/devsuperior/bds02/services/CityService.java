c37c455c0d61612f35955998f165c595e6df1085
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-08-19 19:43:26 -0300

    CityControllerIT Tests delete Ok

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -4,13 +4,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CityService {
@@ -29,6 +33,19 @@ public class CityService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CityDTO(entity);
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Resource not found");
+		}
+		try {
+	        repository.deleteById(id);    		
+		}
+	    catch (DataIntegrityViolationException e) {
+	        throw new DatabaseException("Integrity violation");
+	   	}
 	}
 
 }
